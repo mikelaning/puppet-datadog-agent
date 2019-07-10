0c1ff11a219e39112ad344b1a60d0560a892dbe8
Michael Laning <mike.laning@datadoghq.com>
2019-07-10 10:31:01 -0400

    Create wmi.pp

---
+++ manifests/integrations/wmi.pp
@@ -0,0 +1,26 @@
+
+class datadog_agent::integrations::smi inherits datadog_agent::params {
+  include datadog_agent
+
+  $dst_dir = "${datadog_agent::conf6_dir}/wmi.d"
+
+  file { $dst_dir:
+      ensure  => directory,
+      owner   => $datadog_agent::params::dd_user,
+      group   => $datadog_agent::params::dd_group,
+      #mode    => '0755',
+      require => Package[$datadog_agent::params::package_name],
+      notify  => Exec[$datadog_agent::params::restart_service]
+  }
+  $dst = "${dst_dir}/conf.yaml"
+  
+  file { $dst:
+    ensure  => file,
+    owner   => $datadog_agent::params::dd_user,
+    group   => $datadog_agent::params::dd_group,
+    #mode    => '0600',
+    content => template('datadog_agent/agent-conf.d/wmi.yaml.erb'),
+    require => Package[$datadog_agent::params::package_name],
+    notify  => Exec[$datadog_agent::params::restart_service]
+  }
+}
